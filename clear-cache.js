f6b6c556e29668cf1febb16b9ef2e7c7e5f5ed7e
battama14 <169171805+battama14@users.noreply.github.com>
2026-04-07 01:25:11 +0200

    Delete clear-cache.js

--- clear-cache.js
+++
@@ -1,4 +0,0 @@
-// Colle ce code dans la console du navigateur (F12) pour vider le cache des baleines :
-
-localStorage.removeItem('pls-whales-history');
-console.log('✅ Cache des baleines vidé - Recharge la page');
